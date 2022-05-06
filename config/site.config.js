ee84a2520e624c5f7e4932b1368ac3872161d680
kopscript <kopscript@gmail.com>
2022-05-06 22:03:48 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   title: "OneDriveE5",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your  folder.
-  baseDirectory: '/11111',
+  baseDirectory: '/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
