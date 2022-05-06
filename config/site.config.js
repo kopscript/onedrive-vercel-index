1f091a28182825ad1da698fff0f1edfbe46e93b0
kopscript <kopscript@gmail.com>
2022-05-06 21:39:57 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "OneDrive",
+  title: "OneDriveE5",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your  folder.
   baseDirectory: '/',
